695bca010f804e621c5320e96f2f6351f289252f
Cristhian Cuichan <systems12@logiztikalliance.com>
2023-12-20 05:48:08 -0500

    Cambios de diseño

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import { Header } from "./Componentes/Header";
 import 'boxicons';
 import { BrowserRouter as Router, Route} from "react-router-dom";
-import Paginas from "./Componentes/Paginas";
 function App() {
   return (
     <div className="App">
     <Route>
         <Header/>
-        <Paginas/>
     </Route>
     </div>
   );
